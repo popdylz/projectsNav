2f938c54ee54186f208aa481dfd33feb0127cf7b
MU\Administrator <416018952@qq.com>
2017-03-01 01:22:18 +0800

    no message

---
+++ js/data.js
@@ -0,0 +1,67 @@
+$(function() {
+    var list1 = [{
+        "link": "https://popdylz.github.io/818strategy/index.html",
+        "name": "818发烧攻略"
+    }, {
+        "link": "https://popdylz.github.io/818meetingPlace/index.html",
+        "name": "818会场"
+    }, {
+        "link": "https://popdylz.github.io/bubble/index.html",
+        "name": "气泡"
+    }, {
+        "link": "https://popdylz.github.io/double11-2016/index.html",
+        "name": "2016双11最强攻略"
+    }, {
+        "link": "https://popdylz.github.io/animationShow/index.html",
+        "name": "animation示意"
+    }, {
+        "link": "https://popdylz.github.io/jqueryRotate/index.html",
+        "name": "jqueryrotate--jQuery旋转插件"
+    }],
+    list2 = [{
+        "link": "https://popdylz.github.io/double11-2015/index.html",
+        "name": "2015年双11攻略"
+    }, {
+        "link": "https://popdylz.github.io/december-2015/index.html",
+        "name": "2015年12月攻略"
+    }, {
+        "link": "https://popdylz.github.io/25th-anniversary/index.html",
+        "name": "25周年庆"
+    }, {
+        "link": "https://popdylz.github.io/insectsAwaken-2016/index.html",
+        "name": "2016年惊蛰节"
+    }, {
+        "link": "https://popdylz.github.io/simpleTemplate/index.html",
+        "name": "商品销售简易模版"
+    }, {
+        "link": "https://popdylz.github.io/418shoppingCart/index.html",
+        "name": "418购物车"
+    }, {
+        "link": "https://popdylz.github.io/guidanceForGreenHand/index.html",
+        "name": "新手指导"
+    }, {
+        "link": "https://popdylz.github.io/areaPhone/areaJquery.html",
+        "name": "移动端热区适配"
+    }, {
+        "link": "https://popdylz.github.io/happyValentinesDay/valentine.html",
+        "name": "情人节娱乐"
+    }, {
+        "link": "https://popdylz.github.io/hotSaleList/index.html",
+        "name": "2016双11爆款清单"
+    }];
+
+    new Vue({
+        el: '#arrayLeft',
+        data: {
+            list1: list1
+        }
+    });
+    new Vue({
+        el: '#arrayRight',
+        data: {
+            list2: list2
+        }
+    });
+    
+    
+})
