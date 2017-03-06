b13cd87d25173ef2f6e216c35ef2d70c9a581bb4
MU\Administrator <416018952@qq.com>
2017-03-06 14:37:09 +0800

    no message

--- js/data.js
+++ js/data.js
@@ -1,54 +1,85 @@
 $(function() {
     var list1 = [{
-        "link": "https://popdylz.github.io/818strategy/index.html",
-        "name": "818发烧攻略"
-    }, {
-        "link": "https://popdylz.github.io/818meetingPlace/index.html",
-        "name": "818会场"
-    }, {
-        "link": "https://popdylz.github.io/bubble/index.html",
-        "name": "气泡"
-    }, {
-        "link": "https://popdylz.github.io/double11-2016/index.html",
-        "name": "2016双11最强攻略"
-    }, {
-        "link": "https://popdylz.github.io/animationShow/index.html",
-        "name": "animation示意"
-    }, {
-        "link": "https://popdylz.github.io/jqueryRotate/index.html",
-        "name": "jqueryrotate--jQuery旋转插件"
-    }],
-    list2 = [{
-        "link": "https://popdylz.github.io/double11-2015/index.html",
-        "name": "2015年双11攻略"
-    }, {
-        "link": "https://popdylz.github.io/december-2015/index.html",
-        "name": "2015年12月攻略"
-    }, {
-        "link": "https://popdylz.github.io/25th-anniversary/index.html",
-        "name": "25周年庆"
-    }, {
-        "link": "https://popdylz.github.io/insectsAwaken-2016/index.html",
-        "name": "2016年惊蛰节"
-    }, {
-        "link": "https://popdylz.github.io/simpleTemplate/index.html",
-        "name": "商品销售简易模版"
-    }, {
-        "link": "https://popdylz.github.io/418shoppingCart/index.html",
-        "name": "418购物车"
-    }, {
-        "link": "https://popdylz.github.io/guidanceForGreenHand/index.html",
-        "name": "新手指导"
-    }, {
-        "link": "https://popdylz.github.io/areaPhone/areaJquery.html",
-        "name": "移动端热区适配"
-    }, {
-        "link": "https://popdylz.github.io/happyValentinesDay/valentine.html",
-        "name": "情人节娱乐"
-    }, {
-        "link": "https://popdylz.github.io/hotSaleList/index.html",
-        "name": "2016双11爆款清单"
-    }];
+            "link": "https://popdylz.github.io/projectsNav/index.html",
+            "name": "项目导航"
+        }, {
+            "link": "https://popdylz.github.io/818strategy/index.html",
+            "name": "818发烧攻略"
+        }, {
+            "link": "https://popdylz.github.io/818meetingPlace/index.html",
+            "name": "818会场"
+        }, {
+            "link": "https://popdylz.github.io/bubble/index.html",
+            "name": "气泡"
+        }, {
+            "link": "https://popdylz.github.io/double11-2016/index.html",
+            "name": "2016双11最强攻略"
+        }, {
+            "link": "https://popdylz.github.io/animationShow/index.html",
+            "name": "animation示意"
+        }, {
+            "link": "https://popdylz.github.io/jqueryRotate/index.html",
+            "name": "jqueryrotate--jQuery旋转插件"
+        }, {
+            "link": "https://popdylz.github.io/christmas-2016/index.html",
+            "name": "2016圣诞节"
+        }, {
+            "link": "https://popdylz.github.io/xiaoMi/index.html",
+            "name": "小米超级品牌日"
+        }],
+        list2 = [{
+            "link": "https://popdylz.github.io/double11-2015/index.html",
+            "name": "2015年双11攻略",
+            "img": "double11-2015"
+        }, {
+            "link": "https://popdylz.github.io/december-2015/index.html",
+            "name": "2015年12月攻略",
+            "img": "december-2015"
+        }, {
+            "link": "https://popdylz.github.io/25th-anniversary/index.html",
+            "name": "25周年庆",
+            "img": "25th-anniversary"
+        }, {
+            "link": "https://popdylz.github.io/insectsAwaken-2016/index.html",
+            "name": "2016年惊蛰节",
+            "img": "insectsAwaken-2016"
+        }, {
+            "link": "https://popdylz.github.io/simpleTemplate/index.html",
+            "name": "商品销售简易模版",
+            "img": "simpleTemplate"
+        }, {
+            "link": "https://popdylz.github.io/418shoppingCart/index.html",
+            "name": "418购物车",
+            "img": "418shoppingCart"
+        }, {
+            "link": "https://popdylz.github.io/guidanceForGreenHand/index.html",
+            "name": "新手指导",
+            "img": "guidanceForGreenHand"
+        }, {
+            "link": "https://popdylz.github.io/areaPhone/areaJquery.html",
+            "name": "移动端热区适配",
+            "img": "areaPhone"
+        }, {
+            "link": "https://popdylz.github.io/happyValentinesDay/valentine.html",
+            "name": "情人节娱乐",
+            "img": "happyValentinesDay"
+        }, {
+            "link": "https://popdylz.github.io/hotSaleList/index.html",
+            "name": "2016双11爆款清单",
+            "img": "hotSaleList"
+        }, {
+            "link": "https://popdylz.github.io/redPacket/index.html",
+            "name": "红包",
+            "img": "redPacket"
+        }, {
+            "link": "https://popdylz.github.io/specialPurchasesForTheSpringFestival/index.html",
+            "name": "年货节",
+            "img": "specialPurchasesForTheSpringFestival"
+        }, {
+            "link": "https://popdylz.github.io/theSpringFestivalAtmosphere/index.html",
+            "name": "年味",
+            "img": "theSpringFestivalAtmosphere"
+        }];
 
     new Vue({
         el: '#arrayLeft',
@@ -62,6 +93,5 @@ $(function() {
             list2: list2
         }
     });
-    
-    
+
 })
